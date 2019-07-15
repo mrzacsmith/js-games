51d83d2b3f685e517625e832a55d5a97666f9232
Zac Smith <zrs3141592@gmail.com>
2019-07-14 23:00:43 -0600

    save high scores in localStorage and rank top 5

--- QuizApp/end.js
+++ QuizApp/end.js
@@ -2,6 +2,10 @@ const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+console.log(highScores);
+
+const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 
@@ -13,4 +17,16 @@ username.addEventListener("keyup", () => {
 saveHighScore = e => {
   e.preventDefault();
   // console.log("clicked save");
+
+  const score = {
+    score: Math.floor(Math.random() * 100),
+    // score:
+    name: username.value
+  };
+  highScores.push(score);
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.splice(5);
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  window.location.assign("/");
+  console.log(highScores);
 };
