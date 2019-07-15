ecd47547acfdaffd871f394ece5e3759e41669de
Zac Smith <zrs3141592@gmail.com>
2019-07-14 21:26:41 -0600

    add ./ for end.html

--- QuizApp/game.js
+++ QuizApp/game.js
@@ -54,7 +54,7 @@ getNewQuestion = () => {
   if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
     localStorage.setItem("mostRecentScore", score);
     // go to the end page ~~ game over
-    return window.location.assign("/end.html");
+    return window.location.assign("./end.html");
   }
   questionCounter++;
 
